d4288eea734135c83eb9a5226e08564b13ec5c63
Gizem Diler <gizem.diler@creditwestbank.com>
2021-11-04 14:46:59 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gAng';
+  title: string = 'gAng';
+  user: string = 'me';
+  product1: any = {
+    productId: 1,
+    productName: 'sth1',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product2: any = {
+    productId: 2,
+    productName: 'sth2',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product3: any = {
+    productId: 3,
+    productName: 'sth3',
+    categoryId: 1,
+    unitPrice: 5
+  };
+  product4: any = {
+    productId: 4,
+    productName: 'sth4', 
+    categoryId: 1, 
+    unitPrice: 5
+  };
+  product5: any = { 
+    productId: 5, 
+    productName: 'sth5', 
+    categoryId: 1, 
+    unitPrice: 5 
+  };
+  products = [
+    this.product1,
+    this.product2,
+    this.product3,
+    this.product4,
+    this.product5
+  ];
 }
